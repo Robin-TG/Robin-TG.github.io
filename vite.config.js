e4f26dae8c81121dd2ff095f8740dba98d1577c2
Xantor <xantor1234@gmail.com>
2026-04-04 15:48:33 +0800

    GitHub Pages rollout preparation.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/sieve/',
   plugins: [react()],
   worker: { format: 'es' },
   build: {
